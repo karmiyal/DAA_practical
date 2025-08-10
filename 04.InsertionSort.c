d1a6c2a61a3f60abf415bfe79a97d56429646559
karmiyal <karmiyalbabita@gmail.com>
2025-08-10 22:50:13 +0530

    InsertionSort

--- 04.InsertionSort.c
+++ 04.InsertionSort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
@@ -12,14 +13,15 @@ void insertionSort(int arr[], int n) {
     }
 }
 int main() {
-    int arr[5]={9,3,4,5,2};
-    int size=sizeof(arr)/sizeof(arr[0]);
-    for (int i = 0; i < size; i++) {
+    int n=5;
+    int arr[5];
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    insertionSort(arr, size);
+    insertionSort(arr, n);
     printf("Sorted array: ");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     return 0;
