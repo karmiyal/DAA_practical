2e477a9893eb16f499c9f432a75cf774ce4107df
karmiyal <karmiyalbabita@gmail.com>
2025-08-10 22:46:21 +0530

    LinearSearch

---
+++ 04.InsertionSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        // Shift elements greater than key to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+int main() {
+    int arr[5]={9,3,4,5,2};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    printf("Enter elements: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    insertionSort(arr, size);
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
